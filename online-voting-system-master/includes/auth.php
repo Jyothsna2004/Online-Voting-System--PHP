6840b0e065dbdeb135d14cbcf4d5be14b6935006
Jyothsna2004 <kanugulajyothsna@gmail.com>
2025-07-18 23:35:13 +0530

    Initial commit - customized by Jyothsna

---
+++ online-voting-system-master/includes/auth.php
@@ -0,0 +1,33 @@
+<?php
+// Online Voting System - Authentication Helpers
+// Customized by Jyothsna, 2025
+// Copyright © 2025 Jyothsna. All rights reserved.
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function is_logged_in() {
+    return isset($_SESSION['user_id']);
+}
+
+function require_login() {
+    if (!is_logged_in()) {
+        header('Location: login.php');
+        exit();
+    }
+}
+
+function require_role($role) {
+    if (!is_logged_in() || $_SESSION['role'] !== $role) {
+        header('Location: login.php');
+        exit();
+    }
+}
+
+function logout() {
+    session_unset();
+    session_destroy();
+    header('Location: login.php');
+    exit();
+}
